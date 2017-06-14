a7e3ba348d17f6696cc7b8da7d55d0d4d97227f8
= <undertakers_help@yahoo.com>
2017-06-14 23:39:20 +0200

    POI-List added.

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
-minetest.register_chatcommand("poi_hot", {
-	privs = {
-		interact = true
-	},
-	description = "Teleports you to House of Teleport",
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		player:setpos({x = 10, y = 2.5, z = 55})
-		return true, "Teleportet to HoT"
-	end
-})
+dofile(minetest.get_modpath("poi") .. "/poi.lua")
+
+for i,n in ipairs(points) do
+   
+    -- Register the POI's
+    
+    minetest.register_chatcommand("poi_" .. n.cmd, {
+    privs = n.priv,
+    description = n.desc,
+
+    func = function(name, param)
+	local player = minetest.get_player_by_name(name)
+		player:setpos(n.pos)
+		return true, n.msg
+    end
+    })
+
+end
