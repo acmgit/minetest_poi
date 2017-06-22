d970bf889e531baafda245c0b6619906d461d10d
= <undertakers_help@yahoo.com>
2017-06-22 23:01:55 +0200

    Complete Rewrite of the Mod.

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # Points of Interest
 
 A Mod for Minetest.
-You can define a special Position (POI) and connect it
-with a Chatcommand.
 
-To install this mod, you have to rename the folder from "minetest_poi/" to "poi/".
+This Mod adds PoI's, Point's of Interest to your World.
+If you have set a PoI, you can everytime jump back to the PoI with a simple Chatcommand.
+
+With the Chatcommand /poi_list, you can see everytime a List of all Points of Interest.
+With /poi_list -a you can see it additional with the Coordinates in the World.
+
+To Jump now to the PoI's, use the command /poi <Name>.
+
+If you have the Privs, you can set a Point of Interest with the command /poi_set <Name>.
+And with additional Privs you can delete PoI's with the command /poi_delete <Name>.
+Or you can reload the whole List with the command /poi_reload. 
+
+As Admin, you can grant Privs for the Player.
+All the Points will be stored at a File in your World-Directory with the name: poi.txt
+
+#Privileges
+
+interact:
+/poi <name>
+/poi_list <-a>
+
+tourist:
+/poi_set <name>
+
+tour_guide:
+/poi_delete <name>
+/poi_reload
+
+
+#Install
+
+You have to rename the folder from "minetest_poi/" to "poi/".
 
 #Depends
 
--- init.lua
+++ init.lua
@@ -1,18 +1,133 @@
 dofile(minetest.get_modpath("poi") .. "/poi.lua")
 
-for i,n in ipairs(points) do
-   
-    -- Register the POI's
-    
-    minetest.register_chatcommand("poi_" .. n.cmd, {
-    privs = n.priv,
-    description = n.desc,
+poi = {
+	points = {}
+}
 
-    func = function(name, param)
-	local player = minetest.get_player_by_name(name)
-		player:setpos(n.pos)
-		return true, n.msg
-    end
-    })
+minetest.register_privilege("tourist", "Player set Points of Interest.")
+minetest.register_privilege("tour_guide", "Player may delete all Points of Interest.")
 
+function poi.openlist()
+	local file = io.open(minetest.get_worldpath().."/poi.txt", "r")
+
+	if file then
+		local table = minetest.deserialize(file:read("*all"))
+			if type(table) == "table" then
+				poi.points = table.points
+				return
+			end
+	end
 end
+
+function poi.save()
+	local file = io.open(minetest.get_worldpath().."/poi.txt", "w")
+	if file then
+		file:write(minetest.serialize({
+			points = poi.points
+		}))
+		file:close()
+	end
+end
+ 
+poi.openlist()
+ 
+
+minetest.register_chatcommand("poi_set", {
+	params = "<poi_name>",
+	description = "Set's a Point of Interest.",
+	privs = {tourist = true},
+	func = function(name, poi_name)
+
+		local player = minetest.get_player_by_name(name)
+		local currpos = player:getpos(name)
+
+		poi.points[poi_name] = minetest.pos_to_string(currpos)
+		poi.save()
+				
+		print(name .. " has set the POI: " .. poi_name .. " at " .. minetest.pos_to_string(currpos) .. "\n")
+		return true, "POI: " .. poi_name .. " at " .. minetest.pos_to_string(currpos) .." stored."
+	end,
+})
+
+minetest.register_chatcommand("poi_list", {
+	params = "<-a>",
+	description = "Shows you all Point's of Interest. Optional -a shows you all Point's of Interest with Coordinates.",
+	privs = {interact = true},
+	func = function(name, arg)
+		minetest.chat_send_player(name, "Point's of Interest are:")
+		local list = ""
+		
+		for key, value in pairs(poi.points) do
+
+			if(arg == "-a") then
+				list = list .. key .. ": " .. value .. "\n"
+			
+			else
+				list = list .. key .. "\n"
+			
+			end
+			
+		end
+		
+		minetest.chat_send_player(name, list)		
+		return true
+	end,
+})
+
+minetest.register_chatcommand("poi_delete", {
+	params = "<poi_name>",
+	description = "Deletes a Point of Interest.",
+	privs = {tour_guide = true},
+	func = function(name, poi_name)
+	
+		if(poi_name == nil or poi_name == "") then
+			return false, "POI-Name needed."
+		
+		end
+	
+		local list = ""
+		list = poi_name .. ": " .. poi.points[poi_name]
+
+		poi.points[poi_name] = nil
+		print(name .. " has deleted POI-Name: " .. list .. "\n")
+
+		minetest.chat_send_player(name, list .. " deleted.")
+		poi.save()	-- Write the new list at the server
+		return true
+	end,
+})
+
+minetest.register_chatcommand("poi_reload", {
+	params = "",
+	description = "Loads the List of POI's new.",
+	privs = {tour_guide = true},
+	func = function(name)
+		
+		poi.points = nil
+		poi.openlist()
+		
+		print(name .. " has reloaded the POI-List\n")
+		minetest.chat_send_player(name, "POI-List reloaded.")
+		return true
+	end,
+})
+
+minetest.register_chatcommand("poi", {
+	params = "<POI-Name>",
+	description = "Jumps to the Position of the Point of Interest.",
+	privs = {interact = true},
+	func = function(name, poi_name)
+		
+		local Position = poi.points[poi_name]
+		
+		if(Position == nil or Position == "") then
+			return false, "Unknown Point of Interrest: " .. poi_name .. "."
+			
+		end
+		
+		local player = minetest.get_player_by_name(name)
+		player:setpos(minetest.string_to_pos(Position))
+		return true, "Moved to " .. poi_name .. "."
+
+	end,
+})
